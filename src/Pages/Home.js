974536d8e2d4f7cf68bbfb4b9bc59d20b90c6a71
Randrews1331 <127296519+Randrews1331@users.noreply.github.com>
2024-11-10 17:55:57 +0000

    Nav Bar changes

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,7 +1,9 @@
 import React from 'react';
-import '../CSS/Home.css'; // Import your CSS file for styling
-import logo from '../CSS/Logo.png'; // Import your logo image
-import banner from '../CSS/externalbanner.jpg'; // Import your banner image
+import '../CSS/Home.css';
+import logo from '../CSS/Logo.png';
+import banner from '../CSS/externalbanner.jpg';
+import '@fortawesome/fontawesome-free/css/all.min.css';
+
 
 const Home = () => {
   return (
@@ -10,28 +12,42 @@ const Home = () => {
         <div className="header-top">
           <div className="container">
             <div className="logo">
-              <a href="#"><img src={logo} alt="Greenwich Library Logo" /></a>
+              <a href="#"><img src={logo} alt="AML Logo" /></a>
             </div>
             <nav className="navigation">
               <ul>
-                <li><a href="#">About</a></li>
-                <li><a href="#">Events</a></li>
-                <li><a href="#">Programs</a></li>
-                <li><a href="#">Library Services</a></li>
-                <li><a href="#">Get Involved</a></li>
-                <li><a href="#">Contact</a></li>
+                <li>
+                  <a href="#">
+                    <i className="fas fa-home icon"></i>
+                    Home
+                  </a>
+                </li>
+                <li>
+                  <a href="#">
+                    <i className="fas fa-search icon"></i>
+                    Media Search
+                  </a>
+                </li>
+                <li>
+                  <a href="#">
+                    <i className="fas fa-life-ring icon"></i>
+                    Support
+                  </a>
+                </li>
+                <li>
+                  <a href="#">
+                    <i className="fas fa-sign-in-alt icon"></i>
+                    Log in
+                  </a>
+                </li>
               </ul>
             </nav>
-            <div className="header-search">
-              <input type="text" placeholder="Search the library" />
-              <button type="submit">Search</button>
-            </div>
           </div>
         </div>
         <div className="banner">
           <img src={banner} alt="Greenwich Library Banner" />
           <div className="banner-text">
-            <h1>Welcome to Greenwich Library</h1>
+            <h1>Advance Media Library</h1>
             <p>Explore our collection, programs, and events designed to inspire and educate.</p>
             <a href="#" className="cta-button">Explore More</a>
           </div>
