c5488d9d3579e017faf54fb7449965a70e15e8cc
Randrews1331 <127296519+Randrews1331@users.noreply.github.com>
2024-11-14 12:07:45 +0000

    Home page changes

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,20 +1,61 @@
-// src/components/Home.js
-
-import React from 'react';
+import React, { useState } from 'react';
 import '../CSS/Home.css';
 import logo from '../CSS/Logo.png';
 import banner from '../CSS/externalbanner.jpg';
-import image1 from '../CSS/book 1.jpg'; // Add your image imports
+import image1 from '../CSS/book 1.jpg';
 import image2 from '../CSS/book2.jpg';
-// import image3 from '../CSS/image3.jpg';
-// import image4 from '../CSS/image4.jpg';
+import image3 from '../CSS/book3.jpg';
+import image4 from '../CSS/book4.jpg';
+import image5 from '../CSS/book5.jpg';
+import image6 from '../CSS/book6.jpg';
+import image7 from '../CSS/book7.jpg';
+import image8 from '../CSS/book8.jpg';
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import Navbar from '../Components/Navbar.js';
-import Footer from '../Components/footer.js';
+import Footer from '../Components/footer.js'; // Make sure the path to Footer is correct
 
 const Home = () => {
+  const imageSets = [
+    [image1, image2, image3, image4],
+    [image5, image6, image7, image8]
+  ];
+  const imageSets2 = [
+    [image1, image2, image3, image4],
+    [image5, image6, image7, image8]
+  ];
+  const imageSets3 = [
+    [image1, image2, image3, image4],
+    [image5, image6, image7, image8]
+  ];
+
+  const genres = ["Fiction", "Non-Fiction", "Science", "History"];
+
+  const [currentSetIndex, setCurrentSetIndex] = useState(Array(4).fill(0));
+  const [slideOut, setSlideOut] = useState(Array(4).fill(false));
+
+  const handleNextSet = (rowIndex) => {
+    setSlideOut((prev) => {
+      const newSlideOut = [...prev];
+      newSlideOut[rowIndex] = true;
+      return newSlideOut;
+    });
+
+    setTimeout(() => {
+      setCurrentSetIndex((prev) => {
+        const newSet = [...prev];
+        newSet[rowIndex] = (newSet[rowIndex] + 1) % imageSets.length;
+        return newSet;
+      });
+
+      setSlideOut((prev) => {
+        const newSlideOut = [...prev];
+        newSlideOut[rowIndex] = false;
+        return newSlideOut;
+      });
+    }, 300);
+  };
+
   return (
-    <>
     <div>
       <header className="header">
         <div className="header-top">
@@ -37,37 +78,32 @@ const Home = () => {
       </header>
       <h1 className="mid-text">Media</h1>
 
-      {/* New section with four circles */}
-      <div className="squares-container">
-        <div className="square">
-          <a href="/page1">
-            <img src={image2} alt="Image 1" />
-            <h3>Title 1</h3>
-          </a>
+      {/* Multiple rows of images with different genres */}
+      {genres.map((genre, rowIndex) => (
+        <div key={rowIndex}>
+          <h2 className="genre-title">{genre}</h2>
+          <div className="squares-row">
+            <div className={`squares-container ${slideOut[rowIndex] ? 'slide-left' : ''}`}>
+              {imageSets[currentSetIndex[rowIndex]].map((image, index) => (
+                <div className="square" key={index}>
+                  <a href={`/page${rowIndex * 4 + index + 1}`}>
+                    <img src={image} alt={`Image ${index + 1}`} />
+                    <h3>{`Title ${rowIndex * 4 + index + 1}`}</h3>
+                  </a>
+                </div>
+              ))}
+            </div>
+            <button className="arrow right-arrow" onClick={() => handleNextSet(rowIndex)}>
+              <i className="fas fa-chevron-right"></i>
+            </button>
+          </div>
         </div>
-        <div className="square">
-          <a href="/page2">
-            <img src={image1} alt="Image 2" />
-            <h3>Title 2</h3>
-          </a>
-        </div>
-        <div className="square">
-          <a href="/page3">
-            <img src={image1} alt="Image 3" />
-            <h3>Title 3</h3>
-          </a>
-        </div>
-        <div className="square">
-          <a href="/page4">
-            <img src={image1} alt="Image 4" />
-            <h3>Title 4</h3>
-          </a>
-        </div>
-      </div>
+      ))}
+
+      {/* Include Footer component here */}
       <Footer />
     </div>
-    </>
   );
-}
+};
 
 export default Home;
