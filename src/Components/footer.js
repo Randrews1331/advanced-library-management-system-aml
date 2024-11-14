e5855cdb101ced4827480abd751fc77f7730cca6
RossJennison <c0024074@hallam.shu.ac.uk>
2024-11-14 12:37:30 +0000

    Admin page

--- src/Components/footer.js
+++ src/Components/footer.js
@@ -6,19 +6,19 @@ const Footer = () => {
     <footer>
       <p>&copy; {new Date().getFullYear()} Advanced Media Library</p>
       <ul id="leftList">
-        <li class>About</li>
+        <li class="title">About</li>
         <li><a href="#">Copyrights</a></li>
         <li><a href="#">Contact us</a></li>
         <li><a href="#">ghsdfdfsg</a></li>g
         </ul>    
         <ul>
-          <li><a href="#">dfghjfmd</a></li>
+          <li class = "title">Legal</li>
           <li><a href="#">dfhafgb</a></li>
           <li><a href="#">khsdds</a></li>
           <li><a href="#">dhkgds</a></li>
       </ul>
       <ul id="rightList">
-          <li><a href="#">dfghjfmd</a></li>
+          <li class = "title">dfghjfmd</li>
           <li><a href="#">dfhafgb</a></li>
           <li><a href="#">khsdds</a></li>
           <li><a href="#">dhkgds</a></li>
