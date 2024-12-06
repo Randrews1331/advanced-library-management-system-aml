4f646442d9a628fabad5f4eabdd6694892d3c0c4
RossJennison <c0024074@hallam.shu.ac.uk>
2024-12-06 00:04:13 +0000

    Admin page adjustments
    just minor changes to CSS

--- src/Pages/Admin.js
+++ src/Pages/Admin.js
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import '../CSS/Admin.css';
-import { Link, useHref } from "react-router-dom";
 const Admin = () => {
     const [query, setQuery] = useState("");
     return (
@@ -10,8 +9,8 @@ const Admin = () => {
                 <p>Admin page</p>                       
             </header>
             <div className="searchBar">
-                <label>Search</label>
-                <input type="text" onChange={e => setQuery(e.target.value)} />
+                <label>Search:</label>
+                <input type="text" placeholder="Search stock here" onChange={e => setQuery(e.target.value)} />
             </div>
         </>
     );
